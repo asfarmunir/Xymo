76e5126850531ad667cfe3bf1ad852c6b12ce5f1
asfarmunir <asfarmunir12345@gmail.com>
2024-07-04 01:30:39 +0500

    route added

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,23 +1,24 @@
 import React from "react";
 import Image from "next/image";
 import "./Navbar.css";
+import Link from "next/link";
 const Navbar = () => {
   return (
     <div className=" w-[80%] py-5  flex items-center justify-between nav  ">
       <Image src="/images/logo.png" alt="Hero Image" width={120} height={120} />
       <div className="hidden md:flex gap-6">
-        <a href="#" className="text-primary2 font-semibold">
+        <Link href="#" className="text-primary2 font-semibold">
           Home
-        </a>
-        <a href="#" className=" text-slate-100 font-thin">
-          Projects
-        </a>
-        <a href="#" className=" text-slate-100 font-thin">
+        </Link>
+        <Link href="/features" className=" text-slate-100 font-thin">
+          Features
+        </Link>
+        <Link href="#" className=" text-slate-100 font-thin">
           Services
-        </a>
-        <a href="#" className=" text-slate-100 font-thin">
+        </Link>
+        <Link href="/contact" className=" text-slate-100 font-thin">
           Contact Us
-        </a>
+        </Link>
       </div>
       <button className=" px-3 py-1 bg-primary2 text-white">Book Now</button>
     </div>
